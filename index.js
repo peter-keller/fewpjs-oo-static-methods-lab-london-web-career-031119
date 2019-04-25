1d1fd366a7d99be22d282baa90c0b0f652180ab1
peter-keller <keller.f.peter@gmail.com>
2019-04-25 16:04:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9'\- ]/g, "");
+  }
+
+  static titleize(string) {
+    const arr = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from"
+    ];
+    let res = [];
+    string.split(" ").map((item, index) => {
+      if (index == 0) {
+        res.push(Formatter.capitalize(item));
+      } else {
+        if (arr.includes(item)) {
+          res.push(item);
+        } else {
+          res.push(Formatter.capitalize(item));
+        }
+      }
+    });
+    return res.join(" ");
+  }
 }
